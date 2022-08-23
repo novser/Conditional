a3b6a844e8f5f9e9e1dee3bc8b15ef1c4be3da6d
Sergey Novikov <vizzeb@gmail.com>
2022-08-23 21:22:49 +0300

    init

---
+++ src/main/java/ru/netology/conditional/Config.java
@@ -0,0 +1,22 @@
+package ru.netology.conditional;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.netology.conditional.profile.*;
+
+@Configuration
+public class Config {
+
+    @Bean
+    @ConditionalOnProperty(prefix = "netology", name = "profile.dev", havingValue = "true")
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "netology", name = "profile.dev", havingValue = "false")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+}
